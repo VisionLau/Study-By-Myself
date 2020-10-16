51874e0d613a141c250efd2cb97ae023ee07c941
VisionLau <761979575@qq.com>
2020-10-16 11:02:39 +0800

    Initial commit

---
+++ Design-Mode-Study/Proxy-Pattern/src/main/java/com/vision/test/StaticProxyTest.java
@@ -0,0 +1,41 @@
+package com.vision.test;
+
+public class StaticProxyTest {
+    public static void main(String[] args){
+        new weddingCompany(new who("小明")).marry();
+    }
+}
+
+interface Marry {
+    void marry();
+}
+
+class who implements Marry {
+    private String name;
+    public who(String name) {
+        this.name = name;
+    }
+    @Override
+    public void marry() {
+        System.out.println(name + "结婚了");
+    }
+}
+
+class weddingCompany implements Marry {
+    private Marry whoIsMarry;//谁要结婚
+    public weddingCompany(Marry whoIsMarry) {
+        this.whoIsMarry = whoIsMarry;
+    }
+    @Override
+    public void marry() {
+        befor();
+        whoIsMarry.marry();
+        after();
+    }
+    private void befor() {
+        System.out.println("结婚前，婚庆公司开始筹备");
+    }
+    private void after(){
+        System.out.println("结婚后，婚庆公司收取尾款");
+    }
+}
