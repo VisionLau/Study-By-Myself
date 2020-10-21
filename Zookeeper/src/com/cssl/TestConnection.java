c422b4586a6b940bdfa573c68455c0813fb9b80e
VisionLau <761979575@qq.com>
2020-10-21 16:38:53 +0800

    2020/10/21 新增了zookeeper

---
+++ Zookeeper/src/com/cssl/TestConnection.java
@@ -0,0 +1,44 @@
+package com.cssl;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class TestConnection implements Watcher {
+    private static CountDownLatch   countDownLatch=new CountDownLatch(1);
+    private static ZooKeeper zk=null;
+    private static Stat stat=new Stat();
+
+    public static void main(String[] args) throws Exception {
+        //zookeeper配置数据存放路径
+        String path="/username";
+        //连接zookeeper并且注册一个默认的监听器
+        zk=new ZooKeeper("192.168.17.135:2181",5000,new TestConnection());
+        //等待zk连接成功的通知
+        countDownLatch.await();
+        //获取path目录节点的配置数据，并注册默认的监听器
+        System.out.println(new String(zk.getData(path,true,stat)));
+    }
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if(Event.KeeperState.SyncConnected==watchedEvent.getState()){
+            if(Event.EventType.None==watchedEvent.getType()&&null==watchedEvent.getPath()) {
+                countDownLatch.countDown();
+             }else if(watchedEvent.getType()== Event.EventType.NodeDataChanged){
+                try {
+                    System.out.println("配置已经修改，新值为"+new String(zk.getData(watchedEvent.getPath(),true,stat)));
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
