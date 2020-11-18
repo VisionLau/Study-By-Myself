619b147d69ba725f44397a118e38ef7fd0ce4d06
VisionLau <761979575@qq.com>
2020-11-18 08:52:20 +0800

    2020/11/18 新增了数组的学习

--- Array-Sort-Study/Array/src/main/java/ArrayDemo4.java
+++ Array-Sort-Study/Array/src/main/java/ArrayDemo4.java
@@ -1,10 +1,10 @@
 public class ArrayDemo4 {
     public static void main(String[] args){
         int array[]={10,50,40,60,80};
-        for (int i = 0; i < array.length/2; i++) {
-            int num=array[i];
-            array[i]=array[array.length-1];
-            array[array.length-1]=num;
+        for (int i = 0; i < array.length; i++) {
+            int num=array[i]; //获取角标为i的元素
+            array[i]=array[array.length-1];//角标最大的值赋给角标为i的
+            array[array.length-1-i]=num;//num重新赋回去
         }
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]+",");
