471f5d991fe95ac173c8dc2ac816e1189debdd95
VisionLau <761979575@qq.com>
2020-11-19 10:00:57 +0800

    2020/11/18 新增了数组的学习

---
+++ Array-Sort-Study/Array/src/main/java/ArrayDemo5.java
@@ -0,0 +1,20 @@
+public class ArrayDemo5 {
+    public static void main(String[] args){
+        //二维数组类型  二维数组名 [] []=new 二维数组类型[二维数组长度][二维数组中每个一维数组的长度]
+        //int array[][]=new int[4][2];
+       /* int [][]array=new int[4][2];
+        //int []array[]=new int [4][2];*/
+        int [][]array={{1,23,324},{12,12,3123}};
+        /*for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[array.length-1].length; j++) {
+                System.out.print(array[i][j]+",");
+            }
+        }*/
+        //System.out.println(array);
+        System.out.println(array[array.length-1][array[array.length-1].length-1]);
+      /*  System.out.println(array[0][0]);
+        array[0]=new int[]{2,123,123234};
+        System.out.println(array[0][0]);*/
+
+    }
+}
