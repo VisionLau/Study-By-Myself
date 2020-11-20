6da6f43131cfaf1e735f1f01d43eb03a97156479
VisionLau <761979575@qq.com>
2020-11-20 11:32:34 +0800

    2020/11/18 新增了二维数组的学习

--- Array-Sort-Study/Array/src/main/java/ArrayDemo6.java
+++ Array-Sort-Study/Array/src/main/java/ArrayDemo6.java
@@ -1,19 +1,12 @@
-import jdk.internal.util.xml.impl.Input;
-import sun.security.util.Length;
-
 import java.util.Scanner;
-/*      1
-        1	1
-        1	2	1
-        1	3	3	1
-        1	4	6	4	1
-        1	5	10	10	5	1*/
+
 public class ArrayDemo6 {
     public static void main(String[] args){
         System.out.println("请输入你要打印的行数：");
         Scanner input=new Scanner(System.in);
         int n= input.nextInt();
         int array[][]=new int[n][n];
+
         for (int i = 0; i < array.length; i++) {
             array[i][i]=1;
             array[i][0]=1;
@@ -25,14 +18,13 @@ public class ArrayDemo6 {
             }
         }
 
-
         for (int i = 0; i < array.length; i++) {
+            //打印时消除默认值0
             for (int j = 0; j <=i; j++) {
                 System.out.print(array[i][j]+"\t");
             }
             System.out.println();
         }
-        //
 
     }
 }
